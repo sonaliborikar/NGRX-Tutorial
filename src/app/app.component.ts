0aaf409312072cd2d17da9aa5b10eaf9faf62e9e
SONALI BORIKAR <borikarsonali95@gmail.com>
2022-05-13 17:09:49 +0530

    removed unnecessary component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,18 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  msgOnButtonClick: string | undefined;
-  msgOnChildCompInit: String | undefined;
-
-  name = 'Angular';
-
-  receivedMessageHandler(p: string | undefined) {
-    debugger;
-    this.msgOnButtonClick = p;
-  }
-
-  receiveAutoMsgHandler(p: String | undefined) {
-    debugger;
-    this.msgOnChildCompInit = p;
-  }
+  
 }
